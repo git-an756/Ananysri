ccb44b54c0175dcb34f372a66453e3a133276f7d
git-an756 <101460515+git-an756@users.noreply.github.com>
2022-03-26 20:30:38 +0530

    Add files via upload

---
+++ Assignment_of_Springboot/assignment-3-spring-jdbctemplate/src/main/java/com/bookapp/Assignment501SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment501SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment501SpringRestApplication.class, args);
+	}
+
+}
